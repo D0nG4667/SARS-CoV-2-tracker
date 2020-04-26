a9d7abe1a9624f320201f2a9ecafcbafbc4bbed1
D0nG4667 <osasumwen.okundaye@lifesci.uniben.edu>
2020-04-26 01:21:38 +0100

    Completed Cards

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,15 @@ export const fetchData = async () => {
 
     }
 };
+
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        console.log(data);
+
+    } catch (error) {
+
+    }
+}
